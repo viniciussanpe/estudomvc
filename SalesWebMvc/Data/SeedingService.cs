0d83c3de0cbb5703bec3fe43f238846086b7aa2b
Vinícius Santana <vinicius.sanpe@gmail.com>
2021-03-14 14:57:08 -0300

    Seeding Service

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,41 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return;
+            }
+
+
+            Department d1 = new Department(1, "Compureters");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Fashion");
+            Department d4 = new Department(4, "Books");
+
+            Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", new DateTime(1998, 4, 21), 1000, d1);
+
+            SalesRecord r1 = new SalesRecord(1, new DateTime(2018, 09, 25), 110000.0, Models.Enums.SaleStatus.Billed, s1);
+
+            _context.Department.AddRange(d1, d2, d3, d4);
+            _context.Seller.AddRange(s1);
+            _context.SalesRecord.AddRange(r1);
+            _context.SaveChanges();
+        }
+    }
+}
